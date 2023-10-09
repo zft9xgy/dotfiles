95ed848aa785d6987e008e53b08bb6f8744c916b
zft9xgy <zft9xgy@proton.me>
2023-10-09 22:48:39 +0200

    Adding kitty base

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 
+# copy config files
 cp -r ./hypr/ ~/.config/
-#cp -r ./waybar ~/.config/
+cp -r ./waybar ~/.config/
 cp -r ./wofi ~/.config/
 cp -r ./foot ~/.config/
+cp -r ./foot ~/.config/
+
