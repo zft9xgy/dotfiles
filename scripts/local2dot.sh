8d79667962e4efdbfc2274a9070bd27b4c1a9a1a
zft9xgy <zft9xgy@proton.me>
2023-10-24 07:24:24 +0200

    Updates

--- scripts/local2dot.sh
+++ scripts/local2dot.sh
@@ -5,8 +5,10 @@ LOCAL_PATH=$HOME/.config/
 DOT_PATH=$HOME/shared-across/dotfiles/
 
 #hyprland
-echo $LOCAL_PATH
-echo $DOT_PATH
+echo "Local path:$LOCAL_PATH"
+echo "Dotfiles path:$DOT_PATH"
+echo "Starting copying process..."
+echo ""
 
 #servicios
 # waybar
@@ -16,9 +18,22 @@ echo $DOT_PATH
 # zsh -> en otra ruta
 
 cp -r $LOCAL_PATH/waybar/ $DOT_PATH
+echo "[waybar] done..."
+
 cp -r $LOCAL_PATH/wofi/ $DOT_PATH
+echo "[wofi] done..."
+
 cp -r $LOCAL_PATH/hypr/ $DOT_PATH
+echo "[hypr] done..."
+
 cp -r $LOCAL_PATH/kitty/ $DOT_PATH
+echo "[kitty] done..."
+
 cp -r $LOCAL_PATH/foot/ $DOT_PATH
+echo "[foot] done..."
 
 cp -r $HOME/.zshrc $DOT_PATH/zsh/
+echo "[.zshrc] done..."
+
+echo ""
+echo "[all] done..."
