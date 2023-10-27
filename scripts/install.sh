fad2e23637457ee430050c8acd05d452c41e80b5
zft9xgy <zft9xgy@proton.me>
2023-10-27 15:28:36 +0200

    Install.sh not ready yet

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,25 +1,20 @@
 #!/usr/bin/env bash
 # install config files and reload
 
-
-LOCAL_PATH=$HOME/.config/
-DOT_PATH=$HOME/shared-across/dotfiles/
-
-cp -r $DOT_PATH/waybar/ $LOCAL_PATH
-cp -r $DOT_PATH/hypr/ $LOCAL_PATH
-cp -r $DOT_PATH/kitty/ $LOCAL_PATH
-cp -r $LOCAL_PATH/foot/ $DOT_PATH
-cp -r $LOCAL_PATH/wofi/ $DOT_PATH
-
-cp $DOT_PATH/zsh/ $HOME
+echo 'install script is not ready yet...'
 
 
+# LOCAL_PATH=$HOME/.config/
+# DOT_PATH=$HOME/shared-across/dotfiles/
+
+# cp -r $DOT_PATH/waybar/ $LOCAL_PATH
+# cp -r $DOT_PATH/hypr/ $LOCAL_PATH
+# cp -r $DOT_PATH/kitty/ $LOCAL_PATH
+# cp -r $LOCAL_PATH/foot/ $DOT_PATH
+# cp -r $LOCAL_PATH/wofi/ $DOT_PATH
+
+# cp $DOT_PATH/zsh/ $HOME
 
 
-
-
-
-
-
-sleep 1
-sh ~/shared-across/dotfiles/scripts/reload.sh
+# sleep 0.1
+# sh ~/shared-across/dotfiles/scripts/reload.sh
